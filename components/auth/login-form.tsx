467a8f115db1fd4fc13831f7a7f0dfa47e5b9ecc
Dustin Turner <dustin@dustinbturner.com>
2024-03-13 10:35:54 -0400

    OAuth implemented

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -5,6 +5,7 @@ import * as z from "zod";
 import { useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useSearchParams } from "next/navigation";
 
 import { LoginSchema } from "@/schema"
 import { Input } from "@/components/ui/input"
@@ -25,6 +26,11 @@ import { FormSuccess } from "../form-success";
 import { login } from "@/actions/login";
 
 export const LoginForm = () => {
+        const searchParams = useSearchParams();
+        const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
+            ? "Email already in use with different provider!"
+            : "";
+
         const [error, setError] = useState<string | undefined>("");
         const [success, setSuccess] = useState<string | undefined>("");
         const [isPending, startTransition] = useTransition();
@@ -46,8 +52,10 @@ export const LoginForm = () => {
         startTransition(() => {
         login(values)
         .then((data) => {
-            setError(data.error);
-            setSuccess(data.success);
+            setError(data?.error);
+
+            //TODO: Add when we add 2FA
+            // setSuccess(data?.success);
         });
     });
     }
@@ -104,7 +112,7 @@ export const LoginForm = () => {
                         )}
                     />
                 </div>
-                <FormError message={error} />
+                <FormError message={error || urlError} />
                 <FormSuccess message={success} />
                 <Button
                     disabled={isPending}
@@ -115,7 +123,6 @@ export const LoginForm = () => {
                 </Button>
                 </form>
             </Form>
-            Login Form!
         </CardWrapper>
     )
 }
