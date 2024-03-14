49320c450b828732b9e74fab1222dc8792f123a6
Dustin Turner <dustin@dustinbturner.com>
2024-03-14 09:46:11 -0400

    string literal for the resend confirmation link

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,7 +1,18 @@
 "use client"
 
+import { CardWrapper } from "./card-wrapper";
+
 export const NewVerificationForm = () => {
     return (
-        <NewVerificationForm />
+        <CardWrapper
+            headerLabel= "Confirming your verification"
+            backButtonLabel="Back to login"
+            backButtonHref="/auth/login"
+        >
+            <div className="flex items-center w-full justify-center">
+
+            </div>
+
+        </CardWrapper>
     );
 }
--- lib/mail.ts
+++ lib/mail.ts
@@ -6,13 +6,13 @@ export const sendVerificationEmail = async (
     email: string,
     token: string
 ) => {
-    const confirmLink = 'http://localhost:3000/auth/new-verification?token=${token}';
+    const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
     await resend.emails.send({
         from: "onboarding@resend.dev",
         to: email,
         subject: "Confirm your email",
-        html: '<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>'
+        html: `<p>Click <a href="${ confirmLink }">here</a> to confirm email.</p>`
     });
 
 };
