05a68166cd2bf530571b41a928d2ca5ffdc1b9bd
Dustin Turner <dustin@dustinbturner.com>
2024-03-15 09:39:55 -0400

    fix badge

--- components/user-info.tsx
+++ components/user-info.tsx
@@ -41,7 +41,7 @@ export const UserInfo = ({
                 </div>
                 <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
                     <p className="text-sm font-medium">
-                        EMail
+                        Email
                     </p>
                     <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
                         {user?.email}
